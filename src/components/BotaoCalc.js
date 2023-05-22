39b05c5533bfd0dbe4e5bd4bf5127d939401b17b
Nikolas Raniery <nikolassraniery@gmail.com>
2023-05-22 15:43:14 -0300

    first commit

---
+++ src/components/BotaoCalc.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import styled from 'styled-components'
+
+const BotaoCalc = () => {
+  return (
+    <div>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button><br/>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button><br/>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button><br/>
+        <button>0</button>
+        <button>+</button>
+        <button>-</button>
+        <button>=</button>
+        <button>*</button>
+        <button>/</button>
+    </div>
+  )
+}
+
+export default BotaoCalc
