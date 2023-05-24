d0c99e21a7548bd83d831501b06c0c91f6faed14
Nikolas Raniery <nikolassraniery@gmail.com>
2023-05-24 20:06:20 -0300

    add button funcional

--- src/components/BotaoCalc.js
+++ src/components/BotaoCalc.js
@@ -1,36 +1,33 @@
 import React, { useState } from 'react';
 
 
-
 const BotaoCalc = () => {
 
   let [numero, setNumero] = useState(0);
   let mais = () => {}
   let menos = () => {}
   let reset = () => {}
+  // const one = () => {}
   let number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-  let digito = number.map((digito) => {
-    if (digito === (1)) {
-      setNumero(1);
-    }else{return(0)
-    }
-  });
+  let digito = () => {}
+  // let cache1 = () => {}
+  // let cache2 = () => {}
 
   return (
     <div>
         <h1>{numero}</h1><br/>
-        <button onClick={() => digito(setNumero(digito))}>1</button>
-        <button>2</button>
-        <button>3</button><br/>
-        <button>4</button>
-        <button>5</button>
-        <button>6</button><br/>
-        <button>7</button>
-        <button>8</button>
-        <button>9</button><br/>
-        <button>0</button>
-        <button onClick = {() => mais(setNumero(numero+1))}>+</button>
-        <button onClick = {() => menos(setNumero(numero-1))}>-</button>
+        <button onClick={() => digito(setNumero(number[0]))}>1</button>
+        <button onClick={() => digito(setNumero(number[1]))}>2</button>
+        <button onClick={() => digito(setNumero(number[2]))}>3</button><br/>
+        <button onClick={() => digito(setNumero(number[3]))}>4</button>
+        <button onClick={() => digito(setNumero(number[4]))}>5</button>
+        <button onClick={() => digito(setNumero(number[5]))}>6</button><br/>
+        <button onClick={() => digito(setNumero(number[6]))}>7</button>
+        <button onClick={() => digito(setNumero(number[7]))}>8</button>
+        <button onClick={() => digito(setNumero(number[8]))}>9</button><br/>
+        <button onClick={() => digito(setNumero(number[9]))}>0</button>
+        <button onClick = {() => mais(setNumero(numero+digito))}>+</button>
+        <button onClick = {() => menos(setNumero(numero-digito))}>-</button>
         <button onClick = {() => reset(setNumero(0))}>C</button>
     </div>
   )
