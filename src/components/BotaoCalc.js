89628ee249f9628804a01633719f66b2857d6a2a
Nikolas Raniery <nikolassraniery@gmail.com>
2023-05-29 13:08:59 -0300

    change + and -

--- src/components/BotaoCalc.js
+++ src/components/BotaoCalc.js
@@ -3,34 +3,48 @@ import React, { useState } from 'react';
 
 const BotaoCalc = () => {
 
-  let [numero, setNumero] = useState(0);
-  let mais = () => {}
-  let menos = () => {}
-  let reset = () => {}
-  // const one = () => {}
-  let number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-  let digito = () => {}
-  // let cache1 = () => {}
-  // let cache2 = () => {}
+  const [numero, setNumero] = useState(0);
+  const [valorAnterior, setValorAnterior] = useState(0);
+  const [setResultado] = useState(0);
+  const mais = () => {
+    setValorAnterior(numero);
+    setNumero(0);
+  };
+  const menos = () => {
+    setValorAnterior(numero);
+    setNumero(0);
+  };
+  const reset = () => {
+    setNumero(0);
+  };
+  const digito = (digito) => {
+    setNumero(digito);
+  };
+  const gerarResultado = () => {
+    const soma = valorAnterior + numero;
+    setNumero(soma);
+    const subtracao = valorAnterior - numero;
+  };
 
   return (
     <div>
         <h1>{numero}</h1><br/>
-        <button onClick={() => digito(setNumero(number[0]))}>1</button>
-        <button onClick={() => digito(setNumero(number[1]))}>2</button>
-        <button onClick={() => digito(setNumero(number[2]))}>3</button><br/>
-        <button onClick={() => digito(setNumero(number[3]))}>4</button>
-        <button onClick={() => digito(setNumero(number[4]))}>5</button>
-        <button onClick={() => digito(setNumero(number[5]))}>6</button><br/>
-        <button onClick={() => digito(setNumero(number[6]))}>7</button>
-        <button onClick={() => digito(setNumero(number[7]))}>8</button>
-        <button onClick={() => digito(setNumero(number[8]))}>9</button><br/>
-        <button onClick={() => digito(setNumero(number[9]))}>0</button>
-        <button onClick = {() => mais(setNumero(numero+digito))}>+</button>
-        <button onClick = {() => menos(setNumero(numero-digito))}>-</button>
-        <button onClick = {() => reset(setNumero(0))}>C</button>
+        <button onClick={() => digito(1)}>1</button>
+        <button onClick={() => digito(2)}>2</button>
+        <button onClick={() => digito(3)}>3</button><br/>
+        <button onClick={() => digito(4)}>4</button>
+        <button onClick={() => digito(5)}>5</button>
+        <button onClick={() => digito(6)}>6</button><br/>
+        <button onClick={() => digito(7)}>7</button>
+        <button onClick={() => digito(8)}>8</button>
+        <button onClick={() => digito(9)}>9</button><br/>
+        <button onClick={() => digito(0)}>0</button>
+        <button onClick = {mais}>+</button>
+        <button onClick = {menos}>-</button>
+        <button onClick = {reset}>C</button>
+        <button onClick={gerarResultado}>=</button>
     </div>
-  )
-}
+  );
+};
 
 export default BotaoCalc
