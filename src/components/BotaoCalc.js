d600c979d61ad84cc32e5c8bd3de27e5a35db954
Nikolas Raniery <nikolassraniery@gmail.com>
2023-05-31 11:41:16 -0300

    alt operations

--- src/components/BotaoCalc.js
+++ src/components/BotaoCalc.js
@@ -3,13 +3,18 @@ import React, { useState } from 'react';
 
 const BotaoCalc = () => {
 
+  //Variáveis de Alteração de Estado
   const [numero, setNumero] = useState(0);
   const [valorAnterior, setValorAnterior] = useState(0);
-  const [setResultado] = useState(0);
-  const mais = () => {
+  const [soma, setSoma] = useState(0);
+  const [subtracao, setSubtracao] = useState(0);
+  // const [setResultado] = useState(0);
+
+  //Váriaveis de Operações (+, -, C e =)
+  function mais() {
     setValorAnterior(numero);
     setNumero(0);
-  };
+  }
   const menos = () => {
     setValorAnterior(numero);
     setNumero(0);
@@ -17,32 +22,39 @@ const BotaoCalc = () => {
   const reset = () => {
     setNumero(0);
   };
-  const digito = (digito) => {
-    setNumero(digito);
-  };
-  const gerarResultado = () => {
-    const soma = valorAnterior + numero;
+  let gerarResultado = () => {
+
+    setSoma(valorAnterior + numero);
     setNumero(soma);
-    const subtracao = valorAnterior - numero;
+    
+
+    setSubtracao(valorAnterior - numero);
+    setNumero(subtracao);
+    setSubtracao(0);
+  };
+
+  //Variável Para Aparecerem Vários Digitos
+  const digito = (digito) => {
+    setNumero(numero * 10 + digito);
   };
 
   return (
     <div>
         <h1>{numero}</h1><br/>
-        <button onClick={() => digito(1)}>1</button>
-        <button onClick={() => digito(2)}>2</button>
-        <button onClick={() => digito(3)}>3</button><br/>
-        <button onClick={() => digito(4)}>4</button>
-        <button onClick={() => digito(5)}>5</button>
-        <button onClick={() => digito(6)}>6</button><br/>
-        <button onClick={() => digito(7)}>7</button>
-        <button onClick={() => digito(8)}>8</button>
-        <button onClick={() => digito(9)}>9</button><br/>
-        <button onClick={() => digito(0)}>0</button>
+        <button onClick = {() => digito(1)}>1</button>
+        <button onClick = {() => digito(2)}>2</button>
+        <button onClick = {() => digito(3)}>3</button><br/>
+        <button onClick = {() => digito(4)}>4</button>
+        <button onClick = {() => digito(5)}>5</button>
+        <button onClick = {() => digito(6)}>6</button><br/>
+        <button onClick = {() => digito(7)}>7</button>
+        <button onClick = {() => digito(8)}>8</button>
+        <button onClick = {() => digito(9)}>9</button><br/>
+        <button onClick = {() => digito(0)}>0</button>
         <button onClick = {mais}>+</button>
         <button onClick = {menos}>-</button>
         <button onClick = {reset}>C</button>
-        <button onClick={gerarResultado}>=</button>
+        <button onClick = {gerarResultado}>=</button>
     </div>
   );
 };
